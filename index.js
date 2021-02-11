6dd6f142d7b2a75b188d31d152fe0c7c9035c121
Larry Hawelu <lhawelu@gmail.com>
2021-02-11 10:00:20 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+   names[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+  }
+  return names;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
+
+writeCards(["Lisa", "Brendan", "Ali"], "birthday"); 
+countDown(5);
